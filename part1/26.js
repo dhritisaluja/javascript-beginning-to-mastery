919667d39a332698905ee908cf5e5599fcc8f402
Dhriti Saluja <dhritisaluja1999@gmail.com>
2023-09-11 22:50:56 +0530

    added comments

--- part1/26.js
+++ part1/26.js
@@ -11,14 +11,14 @@
 // console.log(numbers);
 // console.log(fruits[2]);
 let fruits = ["apple", "mango", "grapes"];
-let obj = {}; // object literal
+let obj = {}; // object literal  
 // console.log(fruits);
-// fruits[1] = "banana";
+// fruits[1] = "banana";   //arrays are mutable means when we modify anything, original array will change itself. No new array will be created. 
 // console.log(fruits);
-console.log(typeof fruits);
-console.log(typeof obj);
-console.log(Array.isArray(fruits));
-console.log(Array.isArray(obj));
+console.log(typeof fruits);  //output will be 'object' because type of array is an object. 
+console.log(typeof obj);  //for object literals also we'll get the output as 'object'.
+console.log(Array.isArray(fruits)); //By using this method we can check if our object is an array or an object literal since both have same type. O/P: true
+console.log(Array.isArray(obj)); /* O/P: false  */
 
 
 // array indexing 
