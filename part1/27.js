afe01b6cc4da3662fae10c5ebd0fa6fe5842db4e
Dhriti Saluja <dhritisaluja1999@gmail.com>
2023-09-11 22:57:28 +0530

    added comments

--- part1/27.js
+++ part1/27.js
@@ -5,19 +5,19 @@
 let fruits = ["apple", "mango", "grapes"];
 console.log(fruits);
 // push 
-// fruits.push("banana");
+// fruits.push("banana");  //will add element in the end of the array
 // console.log(fruits);
 // pop 
-// let poppedFruit = fruits.pop();
+// let poppedFruit = fruits.pop();  //remove ele from the end of the array and return it. 
 // console.log(fruits);
 // console.log("popped fruits is", poppedFruit);
 
-// unshift 
-// fruits.unshift("banana");
+// unshift: add element in the start of the array
+// fruits.unshift("banana"); 
 // fruits.unshift("myfruit");
 // console.log(fruits);
 
-// shift 
+// shift: remove ele from the start of the array and return it. 
 // let removedFruit = fruits.shift();
 // console.log(fruits);
 // console.log("removed fruits is ", removedFruit);
