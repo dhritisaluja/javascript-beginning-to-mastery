7c21cdbcff51b6803c6fb2b84fae0f262944e340
Dhriti Saluja <dhritisaluja1999@gmail.com>
2023-09-12 23:54:01 +0530

    use const for creating arrays

--- part1/31.js
+++ part1/31.js
@@ -2,6 +2,10 @@
 
 // heap memory ["apple", "mango"] 0x11
 
-// const fruits = ["apple", "mango"]; // 0x11
-// fruits.push("banana");
-// console.log(fruits);
+const fruits = ["apple", "mango"];       /* fruit is a reference var present in stack memory and it contains the address '0x11' */
+fruits.push("banana");                   /* even though the fruits array is of const type, still banana will be added in the array */
+console.log(fruits);                     /* output will be ["apple", "mango", "banana"]
+
+
+// below statement will give type error as assignment to const variable is not valid
+fruits = ["grapes", "lichi"];   
