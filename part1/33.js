95b6ec461dceceb80a2498cc78b01ad4b942df58
Dhriti Saluja <dhritisaluja1999@gmail.com>
2023-09-13 11:40:09 +0530

    for of loop in array

--- part1/33.js
+++ part1/33.js
@@ -1,12 +1,13 @@
-// for of loop in array
+// for of loop in array: use to directly access the values of an array
 const fruits = ["apple", "mango", "grapes", "fruit4", "fruit5"];
 const fruits2 = [];
 
-// for(let fruit of fruits){
-//     fruits2.push(fruit.toUpperCase());
-// }
-// console.log(fruits2);
+for(let fruit of fruits){
+    fruits2.push(fruit.toUpperCase());
+}
+console.log(fruits2);
 
+//traditional way to access values of an array
 for(let i = 0; i<fruits.length; i++){
     console.log(fruits[i]);
 }
