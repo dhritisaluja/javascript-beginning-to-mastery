61e4fe0a54c47f94fa886216772d4e40de95da05
Dhriti Saluja <dhritisaluja1999@gmail.com>
2023-09-13 16:10:16 +0530

    Intro to objects

--- part1/36.js
+++ part1/36.js
@@ -1,24 +1,25 @@
-// objects reference type  
-// arrays are good but not sufficient 
-// for real world data 
+// object is a reference type  
+// arrays are good but not sufficient to represent real world data 
 // objects store key value pairs 
 // objects don't have index
 
 // how to create objects 
 
-// const person = {name:"Harshit",age:22};
 const person = {
-    name: "harshit",
+    name: "harshit",                                     // or "name" : "harshit" because key is a string. 
     age: 22,
     hobbies: ["guitar", "sleeping", "listening music"]
 }
 console.log(person);
 
 // how to access data from objects 
-// console.log(person["name"]);
-// console.log(person["age"]);
-// console.log(person.hobbies);
+
+console.log(person["name"]);  //bracket notation: here we have used double quotes for accessing key of object because key is always of string type in an object. 
+        /* or */  
+console.log(person.name);
+console.log(person["age"]);
+console.log(person.hobbies);
 
 // how to add key value pair to objects
-person["person"] = "male";
+person.gender = "male";  // or person["gender"] = "male";
 console.log(person);
